c62fae1d2669b9f7851861e57d8e3c641b5e0fee
momothereal <momothereal.mc@gmail.com>
2018-08-16 01:29:13 -0400

    Favicon

--- conf.py
+++ conf.py
@@ -105,6 +105,8 @@ html_static_path = ['_static']
 # Output file base name for HTML help builder.
 htmlhelp_basename = 'Glowstonedoc'
 
+html_favicon = '_static/favicon.ico'
+
 
 # -- Options for LaTeX output ------------------------------------------------
 
